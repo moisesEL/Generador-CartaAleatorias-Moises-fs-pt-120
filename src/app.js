b7d1758bbaa99a12dcf5bf6b662cf6e2dfcce4ea
Moisés <69405573+moisesEL@users.noreply.github.com>
2025-11-14 15:06:40 +0000

    commit final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-import "bootstrap";
 import "./style.css";
 
+window.onload = () => {
+  const palos = ["♦","♥", "♠","♣"];
+  const numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  // guardamos todos los elementos que usemos
+  const carta = document.getElementById("carta");
+  const palo1 = document.querySelector(".palo1");
+  const palo2 = document.querySelector(".palo2");
+  const numero = document.querySelector(".numero");
+  const boton = document.getElementById("generarCarta");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generarCarta() {
+    const numeroAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
+    const paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+
+    carta.classList.remove("corazones", "diamantes", "picas", "treboles");
+
+    if (paloAleatorio === "♥") carta.classList.add("corazones");
+    if (paloAleatorio === "♦") carta.classList.add("diamantes");
+    if (paloAleatorio === "♠") carta.classList.add("picas");
+    if (paloAleatorio === "♣") carta.classList.add("treboles");
+
+    
+    palo1.textContent = paloAleatorio;
+    palo2.textContent = paloAleatorio;
+    numero.textContent = numeroAleatorio;
+  
+  }
+
+  generarCarta();
+
+  // Extra 1: boton generar carta nueva
+  boton.addEventListener("click", generarCarta);
 };
